69c6bb9ffd9b54751bb21c7c3465ae8e84eb8de8
jaredj13 <jaredj9914@hotmail.com>
2024-01-06 15:12:40 -0700

    installed node packages

--- habit-tracker/src/pages/login.jsx
+++ habit-tracker/src/pages/login.jsx
@@ -1,10 +1,6 @@
-import React from "react"
+import React from "react";
+import { useState } from "react";
 
-export default function login (){
-
-    return(
-        <>
-        
-        </>
-    )
+export default function login() {
+  return <></>;
 }
