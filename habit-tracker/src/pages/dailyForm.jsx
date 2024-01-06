ee734a93931404a46b6b461901d4dccea65bea01
jaredj13 <jaredj9914@hotmail.com>
2024-01-06 16:19:37 -0700

    firebase config, database calls file added

--- habit-tracker/src/pages/dailyForm.jsx
+++ habit-tracker/src/pages/dailyForm.jsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React from "react";
+import { Container } from "@mui/material";
 
-export default function dailyForm (){
-
-    return(
-        <>
-        
-        </>
-    )
+export default function dailyForm() {
+  return (
+    <>
+      <Container>
+        <Grid container justifyContent="center">
+          <grid item xs={6}></grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
