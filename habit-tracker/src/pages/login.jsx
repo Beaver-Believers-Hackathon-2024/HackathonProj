93d9dbf3989158c95dc89f4ea108461c2bc43df2
Kai Wang <kaix.wang@mail.utoronto.ca>
2024-01-06 17:46:36 -0700

    catchup

--- habit-tracker/src/pages/login.jsx
+++ habit-tracker/src/pages/login.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import {createUser} from '../firebase/DatabaseCalls'
+import { auth } from '../firebase/FirebaseConfig'
 
 // db function calls
 
 export default function login() {
+  useEffect(()=>{
+    createUser(auth, "test2@gmail.com", "password");
+  }, [])
   return <>hello</>;
 }
