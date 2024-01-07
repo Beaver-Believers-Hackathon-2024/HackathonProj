9e177d4e67bcaebc002bd99d672a08bb0418205b
echiewe <126719218+echiewe@users.noreply.github.com>
2024-01-06 20:12:27 -0700

    dashboard layout

--- habit-tracker/src/pages/dashboard.jsx
+++ habit-tracker/src/pages/dashboard.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import { Grid, Container, Typography } from "@mui/material";
-
-{
-  item ? (
-    <img
-      style={{
-        width: 210,
-        height: 118,
-      }}
-      alt={item.title}
-      src={item.src}
-    />
-  ) : (
-    <Skeleton variant="rectangular" width={210} height={118} />
-  );
-}
+import { Grid, Container, Typography, Button, Avatar } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function dashboard(){
+    const headerStyle = {display: "inline-flex", position: "fixed", top: 20, right: 30};
+    const greetingsStyle = {position: "fixed"}
+    const navigate = useNavigate();
     return <>
-        <Grid container spacing={2}>
-            <Grid xs={12}>
-                WELCOME
-            </Grid>
-        </Grid>
+        <div className="header" style={headerStyle}>
+            <Button
+            onClick={()=>{
+                navigate("/dailyForm")
+            }}
+            variant="contained">
+                Today's Survey
+            </Button>
+            {/* if data .get's date is today, then Review Survey else Today's survey */}
 
-    <Typography variant="h1" component="h2">WELCOME</Typography>
+            <Avatar sx={{marginLeft: 10, bgcolor: "red"}}>U</Avatar>
+        </div>
+
+        <div className="greetings" style={{top: "10%", position: "fixed", alignContent: "center", left: "40%"}}>
+            <Typography 
+            variant="h2">Hello, User.</Typography>
+            <Typography 
+            variant="body1">Let's see some of your recent data.</Typography>
+        </div>
     
     </>;
 }
