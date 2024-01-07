e966939fc2606a06338d57a7107ae7d61e8ec8d8
jaredj13 <jaredj9914@hotmail.com>
2024-01-06 21:50:02 -0700

    fixed db calls

--- habit-tracker/src/firebase/DatabaseCalls.js
+++ habit-tracker/src/firebase/DatabaseCalls.js
@@ -12,68 +12,80 @@ export async function addTestUser() {
     await addDoc(collection(db, 'users'), { userName: 'test2', uid: 'test2222' })
 }
 // create user
-export async function createUser(auth, email, password){
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed up 
-      const user = userCredential.user;
-      const email = userCredential.email;
-      const password = userCredential.password;
+export async function createUser(email, password, username, sex, occupation) {
+    let result = false;
+    await createUserWithEmailAndPassword(auth, email, password, username, sex, occupation)
+        .then((userCredential) => {
+            // Signed up 
+            sessionStorage.setItem('currentUserUID', userCredential.user.uid);
+            writeNewUserData(userCredential.user.email, username, userCredential.user.uid, sex, occupation)
+            result = true
+        })
+        .catch((error) => {
+            console.log(error.code);
+            console.log(error.message);
+        });
+    return (result)
+}
 
+export async function writeNewUserData(email, username, uid, sex, occupation) {
+    await addDoc(collection(db, 'users'), { email: email, userName: username, uid: uid, sex: sex, occupation: occupation }).then(() => {
+        console.log('user added')
+    }).catch((err) => {
+        console.log(err);
     })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
 }
-export async function login(auth, email, password){
-  const auth = getAuth();
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
+
+export async function login(email, password) {
+    let result = false;
+    await signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            sessionStorage.setItem('currentUserUID', userCredential.user.uid);
+            result = true;
+        })
+        .catch((error) => {
+            console.log(error.code);
+            console.log(error.message);
+        });
+    return (result)
 }
+
 export async function logged_in(auth, user) {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
-      navigate('/dailyForm');
-      // ...
-    } else {
-      // User is signed out
-      navigate('/login');
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            const uid = user.uid;
+            navigate('/dailyForm');
+            // ...
+        } else {
+            // User is signed out
+            navigate('/login');
+        }
+    });
+}
+export async function get_User() {
+    const user = auth.currentUser;
+    return user;
+}
+
+export async function log_Out() {
+    try {
+        await signOut(auth)
+        navigate('/login');
+    } catch (error) {
+        console.error('sign out failed');
     }
-  });
-}
-export async function get_User(){
-  const user = auth.currentUser;
-  return user;
 }
 
-export async function log_Out(){
-  try {
-    await signOut(auth)
-    navigate('/login');
-  } catch (error){
-    console.error('sign out failed');
-  }
-}
-
-export async function get_User_Data(user, date){
-  const userDocRef = doc(db, 'users', user, date);
-  try {
-    data = await getDoc(userDocRef);
-    return data;
-  } catch (error){
-    console.log("broke");
-  }
+export async function get_User_Data(user, date) {
+    const userDocRef = doc(db, 'users', user, date);
+    try {
+        data = await getDoc(userDocRef);
+        return data;
+    } catch (error) {
+        console.log("broke");
+    }
 
 }
 
